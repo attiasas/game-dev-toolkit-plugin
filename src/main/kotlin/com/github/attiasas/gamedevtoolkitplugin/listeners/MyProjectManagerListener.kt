8151df074bdefd0e031dd76e4f427294e3764952
GitHub Action <action@github.com>
2023-02-25 09:18:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/attiasas/gamedevtoolkitplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/attiasas/gamedevtoolkitplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.attiasas.gamedevtoolkitplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.attiasas.gamedevtoolkitplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/attiasas/gamedevtoolkitplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/attiasas/gamedevtoolkitplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.attiasas.gamedevtoolkitplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.attiasas.gamedevtoolkitplugin.MyBundle
 
 class MyApplicationService {
 
